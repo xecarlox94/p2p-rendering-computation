699927198f4978b474cc9e48dd2a9cd113747a38
xecarlox94 <jf94.uk@gmail.com>
2025-01-20 22:19:39 +0000

    adding p2prc package

--- Bindings/Haskell/flake.nix
+++ Bindings/Haskell/flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    p2prc.url = "../../";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
