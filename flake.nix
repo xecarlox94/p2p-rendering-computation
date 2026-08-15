6968b62d728cd2cd82cc110c0f5fa919ca65c45a
xecarlox94 <jf94.uk@gmail.com>
2026-08-15 14:32:07 +0100

    updated nix setup

--- flake.nix
+++ flake.nix
@@ -44,11 +44,7 @@
           ];
         };
 
-        # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
-        # This has no effect on other platforms.
-        callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
-
-        p2prcDefault = callPackage ./. { };
+        p2prcDefault = pkgs.callPackage ./. { };
 
       in
       {
