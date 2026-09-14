a6473b2f480b6d4d7d206c12df5d903d7468f46b
xecarlox94 <jf94.uk@gmail.com>
2026-09-14 22:39:58 +0100

    compiler error in rust2go

--- rustlibrary/p2prc_rust_module/flake.nix
+++ rustlibrary/p2prc_rust_module/flake.nix
@@ -19,13 +19,46 @@
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        packages.default = naersk-lib.buildPackage {
-          src = ./.;
-          copyLibs = true;
-          doCheck = true;
+
+        packages = {
+          default = naersk-lib.buildPackage {
+            src = ./.;
+            copyLibs = true;
+            doCheck = true;
+          };
+
+          rust2go = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+            pname = "rust2go";
+            version = "0.4.3";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "ihciah";
+              repo = "rust2go";
+              rev = "master";
+              hash = "sha256-TsIMode5IRjQMEH1S8zSW+KZFslHXcQ4x/+KQCoEq0o=";
+            };
+
+            cargoHash = "sha256-o+58eYowbd+0UQPcty6YUe6kXMA98VRytdZf1kMvoDE=";
+
+            # cargoBuildFlags = [
+            # "-p"
+            # "rust2go-mem-ffi"
+            # ];
+
+            buildNoDefaultFeatures = true;
+            buildFeatures = [
+              # "monoio"
+              "tokio"
+            ];
+
+          });
         };
 
-        defaultPackage = self.packages.${system}.default;
+        # packages.default = naersk-lib.buildPackage {
+        # src = ./.;
+        # copyLibs = true;
+        # doCheck = true;
+        # };
 
         devShells.default =
           with pkgs;
@@ -41,6 +74,8 @@
           };
 
         devShell = self.devShells.${system}.default;
+
+        defaultPackage = self.packages.${system}.default;
       }
     );
 }
